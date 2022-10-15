34e297248bde9494fb11d00ed6bbfc53dd9a4c4f
Temitope <jtemitope384@gmail.com>
2022-10-14 21:06:39 -0700

    stll numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers.
+ *
+ * Return: Alwqys 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
