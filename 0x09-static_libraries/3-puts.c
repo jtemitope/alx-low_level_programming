028b3528a3858c427f42733eb0074f104765ce9c
Temitope <jtemitope384@gmail.com>
2022-11-08 04:32:04 -0800

    creating a library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+/* void _puts(char *str) */
+/* { */
+	/* int count = 0; */
+
+	/* while (count >= 0) */
+	/* { */
+		/* if (str[count] == '\0') */
+		/* { */
+			/* _putchar('\n'); */
+			/* break; */
+		/* } */
+		/* _putchar(str[count]); */
+		/* count++; */
+	/* } */
+/* } */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World!\n");
+	return 0;
+
+}
